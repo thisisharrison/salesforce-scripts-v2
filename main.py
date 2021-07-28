3236607fda8bf8823e32ec3ceb8279a0d95e4b85
Harrison Lau <hlau2@lululemon.com>
2021-07-28 15:30:24 +0800

    add working files

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ def run():
     prompt = "Enter your site preference:\nHK: Hong Kong\nJP: Japan\nUK: EMEA\nAU: AU/NZ\nKR: Korea\n"
     print(prompt)
     site = input("Enter Site > ").strip()
+    
+    # ask user to choose action
+    # create Bot
+    # while True: 
+        # if action picked
+            # take action
+        # prompt new action
+    
+    
     my_bot = SFBot(email, password, twoAuth, site)
     mapping = my_bot.getCategories()
     my_bot.navProducts()
